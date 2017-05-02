54f9394d578a58588b428105781651c1e356fa5c
zhijun_wang <zhijun_wang@kingdee.com>
2017-05-02 18:39:18 +0800

    init twindy-project code

---
+++ twindy-common/src/main/java/twindy/common/exception/BaseException.java
@@ -0,0 +1,7 @@
+package twindy.common.exception;
+
+/**
+ * Created by senola on 2017/5/2 0002.
+ */
+public class BaseException extends RuntimeException {
+}
---
+++ twindy-common/src/main/java/twindy/common/interceptor/AuthInterceptor.java
@@ -0,0 +1,39 @@
+package twindy.common.interceptor;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *  auth 拦截器
+ *  @author senola
+ *  @time 2017-05-02
+ */
+public class AuthInterceptor extends HandlerInterceptorAdapter {
+
+    public AuthInterceptor() {
+        super();
+    }
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        return super.preHandle(request, response, handler);
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        super.postHandle(request, response, handler, modelAndView);
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        super.afterCompletion(request, response, handler, ex);
+    }
+
+    @Override
+    public void afterConcurrentHandlingStarted(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        super.afterConcurrentHandlingStarted(request, response, handler);
+    }
+}
