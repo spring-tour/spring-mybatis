0f47f3017eaa1765f3d033f3886aca91c6d34d8c
zhijun_wang <zhijun_wang@kingdee.com>
2017-05-11 18:07:34 +0800

    init exception code

---
+++ twindy-admin/src/main/java/twindy/admin/entity/Admin.java
@@ -0,0 +1,7 @@
+package twindy.admin.entity;
+
+/**
+ * Created by senola on 2017/5/8 0008.
+ */
+public class Admin {
+}
--- twindy-dataModel/twindy.sql
+++ twindy-dataModel/twindy.sql
@@ -20,3 +20,26 @@ CREATE TABLE `t_user` (
   KEY `IDX_EMAIL` (`email`) USING BTREE,
   KEY `IDX_IPHONE_NO` (`phoneNo`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- admin
+DROP TABLE IF EXISTS `t_admin`;
+CREATE TABLE `t_admin` (
+  `id` varchar(100) NOT NULL COMMENT '用户主键',
+  `username` varchar(100) DEFAULT NULL COMMENT '用户名',
+  `nickname` varchar(100) DEFAULT NULL COMMENT '昵称',
+  `password` varchar(100) DEFAULT NULL COMMENT '密码',
+  `email` varchar(100) DEFAULT NULL COMMENT '邮箱',
+  `phoneNo` varchar(20) DEFAULT NULL COMMENT '手机号',
+  `avatar` varchar(255) DEFAULT NULL COMMENT '头像地址',
+  `role` int(11) DEFAULT '0' COMMENT '用户角色（0：admin, 1:一般用户）',
+  `registerTime` datetime DEFAULT NULL COMMENT '注册时间',
+  `loginTimes` int(11) DEFAULT NULL COMMENT '登录次数',
+  `status` int(11) DEFAULT '0' COMMENT '用户状态（0：正常）',
+  `updateTime` datetime DEFAULT NULL COMMENT '修改时间',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UDX_ID` (`id`) USING BTREE,
+  KEY `IDX_USERNAME` (`username`) USING BTREE,
+  KEY `IDX_EMAIL` (`email`) USING BTREE,
+  KEY `IDX_IPHONE_NO` (`phoneNo`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
